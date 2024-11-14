3e30d3a35fceb9f49521826557b0202f42f7ee51
Sully Sullenberger <sully@sadburger.com>
2024-11-14 11:13:05 -0800

    setup

---
+++ content/Hello-world.md
@@ -0,0 +1,5 @@
+Title: Hello World
+Date: 2024-11-14 10:00
+Category: Blog
+
+Hello...
---
+++ pelicanconf.py
@@ -0,0 +1,55 @@
+AUTHOR = 'Sully'
+SITENAME = "Sully's Blog"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+PLUGINS = [
+    'neighbors', # find next, previous article
+]
+
+# Social widget
+SOCIAL = (
+    ("linkedin", "https://www.linkedin.com/in/msullenberger/"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 25
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+OUTPUT_PATH = "blog-output"
+DELETE_OUTPUT_DIRECTORY = True
+
+STATIC_PATHS = ["images"]
+# PAGE_PATHS = [
+#     'pages'
+# ]
+# DIRECT_TEMPLATES = [
+#     'index',
+#     'authors',
+#     'categories',
+#     'tags',
+#     'archives',
+#     '404',
+# ]
